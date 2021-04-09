f99cabcd08870d0d98dc96d0d6312ac647888e51
Ivan <64414894+ivanxc@users.noreply.github.com>
2021-04-09 16:11:36 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-#include <cstdlib> // для system
 using namespace std;
 
 int main() 
 { 
-    cout << "Hello, world!" << endl;
+    std::cout << "Hello, world!" << std::endl;
     return 0; 
 }
